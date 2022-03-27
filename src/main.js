8c8c4fa01a28aaa16d07f98dc12a1f182c9290d7
yuomatsu <07yuya08@gmail.com>
2022-03-27 15:09:01 +0900

    カスタムディレクティブ終了

--- src/main.js
+++ src/main.js
@@ -4,6 +4,17 @@ import LikeNumber from "./components/LikeNumber.vue"
 
 Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber)
+// Vue.directive("border", function (el, binding) {
+//   el.style.borderWidth = binding.value.width;
+//   el.style.borderColor = binding.value.color;
+//   el.style.borderStyle = binding.arg;
+//   if (binding.modifiers.round) {
+//     el.style.borderRadius = "0.5rem"
+//   }
+//   if (binding.modifiers.shadow) {
+//     el.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.26)"
+//   }
+// })
 
 new Vue({
   render: h => h(App),
