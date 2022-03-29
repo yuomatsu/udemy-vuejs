64afb36ddd689b37b97013537c0cfef712662615
yuomatsu <07yuya08@gmail.com>
2022-03-29 15:10:51 +0900

    v-modelをVuexで使う、別ファイル書き出し

---
+++ src/store/modules/count.js
@@ -0,0 +1,24 @@
+const state = {
+    count: 2
+};
+const getters = {
+    doubleCount: state => state.count * 2,
+    tripleCount: state => state.count * 3,
+};
+const mutations = {
+    increment(state, number) {//stateの値を帰る処理 
+        state.count += number
+    }
+};
+const actions = {
+    increment({ commit }, number) {
+        commit("increment", number);
+    }
+};
+
+export default {
+    state,
+    getters,
+    mutations,
+    actions
+}
--- src/views/HomeTest2.vue
+++ src/views/HomeTest2.vue
@@ -1,22 +1,34 @@
 <template>
   <div>
     <h1>HOMEです。。</h1>
-    <button @click="doubleCounter">+</button>
+    <button @click="increment(4)">+</button>
     <p>{{ doubleCount }}</p>
     <p>{{ tripleCount }}</p>
+		<input type="text" v-model="message">
+		<p>{{ message }}</p>
   </div>
 </template>
 
 <script>
+import {mapActions} from "vuex"
 import { mapGetters } from "vuex";
+// import { mapMutations } from "vuex";
 export default {
   computed: {
     ...mapGetters(["doubleCount", "tripleCount"]),
+		message: {
+			get() {
+				return this.$store.getters.message;
+			},
+			set(value) {
+				this.$store.dispatch("updateMessage", value)
+			}
+		}
   },
   methods: {
-    doubleCounter() {
-      this.$store.commit("increment", 2);
-    },
+    // ...mapMutations(["increment"]),
+		...mapActions(["increment"])
   },
+
 };
 </script>
